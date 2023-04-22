9baf091801d7bab7f6479bd3046aac4d0baab526
JohnnyNguyen579 <119677134+JohnnyNguyen579@users.noreply.github.com>
2023-04-22 10:58:17 +0700

    update tablet ,mobile

--- assets/css/js/swiper.js
+++ assets/css/js/swiper.js
@@ -30,7 +30,7 @@ var swiper = new Swiper(".container-slider__slide", {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
-  }
+  },
 });
 
 var swiper = new Swiper(".content-container__slide-1", {
@@ -44,7 +44,16 @@ var swiper = new Swiper(".content-container__slide-1", {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
-  }
+  },
+
+  breakpoints: {
+    0:{spaceBetween: 4,
+          slidesPerView: 2},
+    800:{spaceBetween: 4,
+          slidesPerView: 3},
+    1024:{spaceBetween: 20,
+          slidesPerView: 4}
+  },
 });
 
 var swiper = new Swiper(".content-container__slide-2", {
@@ -55,4 +64,27 @@ var swiper = new Swiper(".content-container__slide-2", {
     nextEl: ".content-slider__button-next",
     prevEl: ".content-slider__button-prev",
   },
+  breakpoints: {
+    0:{spaceBetween: 4,
+          slidesPerView: 2},
+    800:{spaceBetween: 4,
+          slidesPerView: 3},
+    1024:{spaceBetween: 20,
+          slidesPerView: 4}
+  },
+});
+
+var swiper = new Swiper(".container-slider__slide-3", {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".content-container__slide-3", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 20,
 });
